b0267697a87f8788919504000746a3ebf0401e92
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2023-02-22 23:52:00 +0100

    birds CRUD

--- 02_birds_api/app.js
+++ 02_birds_api/app.js
@@ -18,6 +18,19 @@ const birds = [
     name: "duck",
   },
 ];
+
+//id emulator
+const ids = [1, 2, 3, 4];
+
+const handleNewId = () => {
+  const newId = ids.length + 1;
+  ids.push(newId);
+  console.log(newId);
+  return newId;
+}
+
+const getIndex = (i) => birds.findIndex(bird => bird.id === +i);
+
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -26,16 +39,27 @@ app.get("/birds", (req, res) => {
   res.send(birds);
 });
 
-//
-
 app.get("/birds/:id", (req, res) => {
-  console.log(req.params);
-  birds.forEach((bird) => {
-    if (bird.id == req.params.id) {
-      console.log(bird);
-      res.send(bird);
-    }
-  });
+  const birdById = birds.filter(bird => bird.id === +req.params.id);
+  res.send(birdById);
 });
+
+app.post("/birds", (req, res) => {
+  const newBird = { id: handleNewId(), name: req.body.name };
+  birds.push(newBird);
+  res.send(newBird);
+});
+
+app.delete("/birds/:id", (req, res) => {
+  const id = + req.params.id;
+  birds.splice(getIndex(id), 1);
+  res.send({message: `bird ${id} deleted`})
+});
+
+app.put("/birds/:id", (req, res) => {
+  const id = + req.params.id;
+  birds.splice(getIndex(id), 1, {id: id, name: req.body.name})
+  res.send(birds.filter(bird => bird.id === +req.params.id))
+})
 
 app.listen(8080);
