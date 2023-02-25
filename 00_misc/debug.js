6f250c84d3a2bbca0fdfeb42f85d64fd39831c92
brrobert17 <73032043+brrobert17@users.noreply.github.com>
2023-02-25 13:26:32 +0100

    vercel fix?

---
+++ 00_misc/debug.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const myConstant = "Hello World";
+
+eval(x === 2);
+let x = 3;
+
+var public = "public variable";
+
+const defaultValue = null;
+
+const message = "";
+
+let console = "Output";
+
+delete console;
+
+delete arguments[0];
+
+const arguments = [1, 2, 3];
+
+function alerting() {
+    message = console;
+ }
+
+ delete 
+ 
+
+function searchingForTrouble() {
+    trouble = true;
+}
+
+function logMessage(guard="") {
+    message = "Everything is fine.";
+    if (!guard) {
+        // The task is to print "Everything is fine." here.
+        console.log(message);
+    }
+}
+
+logMessage();
+
+everythingIsFine(true); 
+
+
+
+
--- 01_first_server/app.js
+++ 01_first_server/app.js
@@ -2,63 +2,75 @@
 //same in 1 line
 const express = require("express");
 const app = express();
-app.use(express.json())
-
+app.use(express.static("public"));
 
 // route (entire thing)
 // HTTP method
 // | endpoint  callback function
 app.get("/", (req, res) => {
-    res.send({ message: "Our first route"})
+  res.send({ message: "Our first route" });
 });
 
 let bicycleSpins = 0;
 app.get("/spinthebicycle", (req, res) => {
-    bicycleSpins += 1;
-    res.send({message: `people have spun ${bicycleSpins} times` })
+  bicycleSpins += 1;
+  res.send({ message: `people have spun ${bicycleSpins} times` });
 });
 
 let isKicked = false;
 app.get("/kickthedinosaur", (req, res) => {
-    isKicked = true;
-    if (isKicked) {
-        res.send({message: "ow ow ow"})
-    }
-    else {
-        res.send({message: "ha ha ha"})
-    }
-    isKicked = false;
-})
+  isKicked = true;
+  if (isKicked) {
+    res.send({ message: "ow ow ow" });
+  } else {
+    res.send({ message: "ha ha ha" });
+  }
+  isKicked = false;
+});
 
 app.get("/about", (req, res) => {
-    res.send(`
+  res.send(`
     <h1 style="color: blue;">hello world</h1>
-    `)
-})
+    `);
+});
 
 app.get("/bat", (req, res) => {
-
-    console.log(req.query);
-    res.send({
-        message: `The bat is ${req.query.adjective}`
-    });
+  console.log(req.query);
+  res.send({
+    message: `The bat is ${req.query.adjective}`,
+  });
 });
 
 app.get("/bottle/:bottleSize", (req, res) => {
-
-    console.log(req.params);
-    res.send({
-        message: `The bottle is ${req.params.bottleSize}`
-    });
+  console.log(req.params);
+  res.send({
+    message: `The bottle is ${req.params.bottleSize}`,
+  });
 });
 
 app.post("/package", (req, res) => {
-
-    console.log(req.body);
-    res.send(
-       req.body
-    );
+  console.log(req.body);
+  res.send(req.body);
 });
 
-app.listen(8080);
+app.get("/time", (req, res) => {
+  res.send({ 
+    timeUTC: new Date(), 
+    timeLocal: Date(), 
+    unixTimeStamp: Date.now() });
+});
 
+app.get("/time/day", (req, res) => {
+    res.send({ 
+      day: Intl.DateTimeFormat("en-us", {weekday: "long"}).format(new Date())
+  });
+});
+
+app.get("/time/month", (req, res) => {
+    res.send({ 
+      month: Intl.DateTimeFormat("en-us", {month: "long"}).format(new Date())
+    });
+});
+
+
+app.listen(8080);
